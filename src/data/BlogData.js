9b8af98b70ada1dbbd87825d53f65800e0798a49
vaibhav <vaibhavmehra5154@gmail.com>
2024-04-11 21:26:28 +0530

    third

---
+++ src/data/BlogData.js
@@ -0,0 +1,25 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Adaan Digital Solutions",
+        tags:["Web Developer", "Duda Developer", "CMS"],
+        date:"1 September, 2022 - 15 April, 2023",
+        imgSrc:"https://images.crunchbase.com/image/upload/c_pad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/o3pestmfgwcwmghtqd4v",
+    },
+    {
+        id:2,
+        name:"2Digits Innovations",
+        tags:["UI/UX Designer","Tester", "Development", "Business Analytics"],
+        date:"1 July, 2023 - Till Now",
+        imgSrc:"https://www.2digitinnovations.com/2digitinnovations_logo.png",
+    },
+    {
+        id:3,
+        name:"Freelance",
+        tags:["UI/UX Design","Development"],
+        date:"",
+        imgSrc:"https://img.freepik.com/free-vector/gradient-ui-ux-background_23-2149051557.jpg?w=1380&t=st=1712844813~exp=1712845413~hmac=e3643608b301b8d0fa26fdcd4685d9183c86a891b81aefa42b64826e7d38386d",
+    },
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Home Rent Website",
+        description:"It is build on Figma and the clients wants the basic designs for the website for its rent house services.",
+        tags:["Figma","UI Design"],
+        
+        demo:"https://www.figma.com/file/KQgQ4FzL5hFIJXdRzVaFpP/Rent-Home?type=design&node-id=1%3A2&mode=design&t=hkjF6ASUglTcIvQo-1",
+        github:"https://www.figma.com/file/KQgQ4FzL5hFIJXdRzVaFpP/Rent-Home?type=design&node-id=1%3A2&mode=design&t=hkjF6ASUglTcIvQo-1"
+    },
+    {
+        id:2,
+        name:"Art Gallery Website Design",
+        description:"This is a website design created for an art event so that the user can interact and see the website first before the visit.",
+        tags:["Figma","UI Design"],
+        
+        demo:"https://www.figma.com/file/7LeLxEpjmMVdGdTLCm7PY1/Art-gallery?type=design&node-id=1%3A907&mode=design&t=PNN59OpuFpELa1td-1",
+        github:"https://www.figma.com/file/7LeLxEpjmMVdGdTLCm7PY1/Art-gallery?type=design&node-id=1%3A907&mode=design&t=PNN59OpuFpELa1td-1"
+        
+    },
+    {
+        id:3,
+        name:"Dashboar Design",
+        description:"This is a Dashboard Design, it's a template how we can use or design a dashboard according to your usecase.",
+        tags:["Figma","UI Design"],
+        
+        demo:"https://www.figma.com/file/0VwagZqrKS8LM3PXXT45yi/Dashboard-Design?type=design&node-id=1%3A105&mode=design&t=o1ehorYLMURjaPqK-1",
+        github:"https://www.figma.com/file/0VwagZqrKS8LM3PXXT45yi/Dashboard-Design?type=design&node-id=1%3A105&mode=design&t=o1ehorYLMURjaPqK-1"
+    },
+    {
+        id:4,
+        name:"Rider Event Scheduler",
+        description:"This is a mobile application for a rider who can organise and schedule an event.",
+        tags:["Figma","UI Design"],
+        
+        demo:"https://www.figma.com/file/QhRt63jt2BKfzFeyTeRj5y/Rider-Event-Scheduler?type=design&node-id=1%3A49&mode=design&t=pIo5EkRg0NhxeDqN-1",
+        github:"https://www.figma.com/file/QhRt63jt2BKfzFeyTeRj5y/Rider-Event-Scheduler?type=design&node-id=1%3A49&mode=design&t=pIo5EkRg0NhxeDqN-1"
+       
+    },{
+        id:5,
+        name:"Wakaalert",
+        description:"This is an application used when you are in trouble. You can share your location with your guardian or your loved ones.",
+        tags:["Figma","UI Design"],
+        
+        demo:"https://www.figma.com/file/DTJao6liRbnWf4nTs0SzKu/Waka-alert?type=design&node-id=1%3A17&mode=design&t=FZGEOyFCMnc4EBJT-1",
+        github:"https://www.figma.com/file/DTJao6liRbnWf4nTs0SzKu/Waka-alert?type=design&node-id=1%3A17&mode=design&t=FZGEOyFCMnc4EBJT-1"
+        
+    },
+    {
+        id:6,
+        name:"Case study for Foodhub App",
+        description:"This is a case study for the food delivery application, I have solved the UI and UX problems in it.",
+        tags:["Figma","UI Design"],
+        
+        demo:"https://www.figma.com/file/gRIt2mYiMkgWWXAwRsyBoV/Food-app-Restaurant?type=design&node-id=0%3A632&mode=design&t=yF5NueDbYvvm4Ape-1",
+        github:"https://www.figma.com/file/gRIt2mYiMkgWWXAwRsyBoV/Food-app-Restaurant?type=design&node-id=0%3A632&mode=design&t=yF5NueDbYvvm4Ape-1"
+        
+    },
+    
+  
+] 
